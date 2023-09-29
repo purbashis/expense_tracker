010f2c03dff56614522c82c2526e86ccdd51778f
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-09-29 21:54:54 +0530

    _

---
+++ expense_tracker/lib/expenses.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+    // TODO: implement createState
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("The chart"),
+          Text("Expense..."),
+        ],
+      ),
+    );
+  }
+}
--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:expense_tracker/expenses.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: ,
+      home: Expenses(),
     ),
   );
 }
