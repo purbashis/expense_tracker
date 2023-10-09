bb40cbbe3a7f6da4a9797786cfb1ef62dc9f3a96
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-10-09 14:07:03 +0530

    for long list view we used ListView widget

--- expense_tracker/lib/expenses_list.dart
+++ expense_tracker/lib/expenses_list.dart
@@ -1,15 +1,21 @@
-import 'package:flutter/cupertino.dart';
 import 'package:expense_tracker/expense.dart';
+import 'package:flutter/material.dart';
 
-class ExpensesList extends StatelessWidget{
-const ExpensesList({super.key, })
 
-final List<Expenses> expenses;
 
-  @Override
-  Widget build (BuildContext context){
-    return Column(
-      
-    )
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Text(expenses[index].title),
+    );
   }
 }
