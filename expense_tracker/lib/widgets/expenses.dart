b7bfad579d3557367f1ecfb01861cb781d43b8e3
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-10-17 13:50:43 +0530

    handling user text input

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
@@ -28,16 +29,22 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, 
+        builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker '),
         actions: [
-        
-        IconButton(icon: const Icon(Icons.add),
-        onPressed: (){})
-      ],),
+          IconButton(
+              icon: const Icon(Icons.add), onPressed: _openAddExpenseOverlay)
+        ],
+      ),
       body: Column(
         children: [
           //toolbar with the add button = > Row ()
