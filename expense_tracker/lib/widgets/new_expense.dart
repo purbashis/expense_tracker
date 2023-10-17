4da18c74b6cb1e629a24c39453878e143f2523b6
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-10-17 21:20:13 +0530

    setState() for pickedDate and fixed the date modal

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
@@ -12,17 +13,21 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
 
-  void _presentDatePicker() {
+  void _presentDatePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, now.day);
-
-    showDatePicker(
+    final pickedDate = await showDatePicker(
         context: context,
         initialDate: now,
         firstDate: firstDate,
         lastDate: now);
+         setState(() {
+      _selectedDate = pickedDate;
+    });
   }
+ 
 
   @override
   void dispose() {
@@ -65,12 +70,13 @@ class _NewExpenseState extends State<NewExpense> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text('Selected Date'),
+              Text(_selectedDate == null ?'No date selected ': formatter.format(_selectedDate!),),
                     IconButton(
                         onPressed: _presentDatePicker,
                         icon: const Icon(
                           Icons.calendar_month,
                         ))
+
                   ],
                 ),
               )
