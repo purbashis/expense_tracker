e9caab86b7d02eb825becf399a15ae29b38cc44b
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-10-17 08:27:16 +0530

    using icons and formatted date

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -1,23 +1,36 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
-enum Category {
-  food ,travel ,leisure , work 
-}
+enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff_sharp,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work
+};
 
 class Expense {
-  Expense({required this.title, 
-  required this.amount,
-   required this.date,
-   required this.category,})
-
-      : id = uuid.v4();
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
-  final Category category;//
+  final Category category; //
 
+  String get formattedDate {
+    return formatter.format(date);
+
+  }
 }
