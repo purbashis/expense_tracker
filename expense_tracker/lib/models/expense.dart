b300704b5f255cd7d7869610385fbe4a809e8fc5
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-10-22 15:50:51 +0530

    adding alternative constructor functions and filtering list for the chart widgets

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
@@ -31,6 +32,25 @@ class Expense {
 
   String get formattedDate {
     return formatter.format(date);
+  }
+}
 
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense>allExpenses,this.category): expenses= allExpenses.where((expense) => expense.category==category).toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum = sum + expense.amount;// 
+    }
+    return sum;
   }
 }
